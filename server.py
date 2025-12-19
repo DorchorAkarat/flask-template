73662577178e4f07389e85a77a065632806c4af4
Akarat <akarat0825@gmail.com>
2025-12-19 16:53:38 +0700

    add movie

--- server.py
+++ server.py
@@ -9,21 +9,21 @@ def index():
 @app.route('/about')
 def about():
     title='About Page'
-    name='Aekkachat'
-    email='aekkachat@example.com'
-    phone='0812345678'
+    name='Akarat'
+    email='akarat555@gmail.com'
+    phone='0963705756'
     return render_template('about.html', title=title,  name=name, email=email, phone=phone)
 
 @app.route('/favoerite/food')
 def favorite_food():
     title='Favorite Food'
-    foods = ['Pizza', 'Sushi', 'Ice Cream', 'Tacos']
+    foods = ['ข้าวมันไก่', 'ข้าวผัด', 'ข้าวต้ม', 'ผัดมาม่า']
     return render_template('food.html', foods=foods, title=title)
 
 @app.route('/favorite/sport')
 def favorite_sport():
     title='Favorite Sport'
-    sports = ['บอล', 'บาสเกตบอล', 'วอลเลย์บอล', 'ว่ายน้ำ']
+    sports = ['แบทมินตัน', 'บาสเกตบอล', 'วิ่ง', 'ปั่นจักรยาน']
     return render_template('sport.html', sports=sports, title=title)
 
 @app.route('/greet/<username>')
@@ -34,5 +34,5 @@ def wlcome(username):
 @app.route('/square/Favorite_movie')
 def favorite_movie():
     title='Favorite Movie'
-    movies = ['Inception', 'The Matrix', 'Interstellar', 'The Dark Knight', 'Forrest Gump']
+    movies = ['Now You See Me', 'Machete Kill', 'Breaking Bad', 'Lacasa de papel', 'Predator']
     return render_template('movie.html', movies=movies, title=title)
